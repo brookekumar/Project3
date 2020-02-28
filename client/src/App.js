6fd0ac8b21e93d8b3db9bc1b0db0aea8bf0c58f6
tasha tran <tashatran@tashas-MacBook-Air.local>
2020-02-27 18:55:13 -0600

    created HeaderMain component to clean up app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import './App.css';
-import Card from "./components/Card"
+import Card from "./components/Card";
+import HeaderMain from"./components/HeaderMain";
 
 function App() {
   return (
-    <div>
-       <h1 class="center grey-text">Expanding Card</h1>
-<p class="center blue-text">React Mobile UI Pratice， Click Picture to expand, then click content to expand more</p>
-<div style={{display:"flex"}}>
+   
+   <div>
+       
+       <HeaderMain/>
+    
+        <div style={{display:"flex"}}>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        </div>
+        
+        <br/>
 
-<Card/>
-<Card/>
-<Card/>
-<Card/>
-<Card/>
-</div>
-</div>
+        <div style={{display:"flex"}}>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        </div>
+
+    </div>
   )
 }
 
---
+++ client/src/components/HeaderMain.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function HeaderMain(){
+
+    return(
+        <div>
+        
+         <h1 class="center grey-text"> <a href="https://imgur.com/CBfRgAG"><img src="https://i.imgur.com/CBfRgAG.png" title="source: imgur.com" /></a> </h1>
+         
+         <p class="center blue-text"> insert introduction here </p>
+        </div>
+
+    )
+}
+
+export default HeaderMain;
